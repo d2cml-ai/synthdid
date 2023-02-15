32e30fae2ad4fa7e7d817af555848fe9743ff259
tjhon <fr.jhonk@gmail.com>
2023-02-15 17:24:01 -0500

    base for print

--- src/fromat.jl
+++ src/fromat.jl
@@ -1,10 +1,9 @@
+include("setup.jl")
 
-# format.synthdid_estimate = function(x, ...) {
-#   info = summary(x, fast=TRUE)
-#   d = as.list(info$dimensions)
-#   sprintf('synthdid: %1.3f +- %1.3f. Effective N0/N0 = %1.1f/%d~%1.1f. Effective T0/T0 = %1.1f/%d~%1.1f. N1,T1 = %d,%d.',
-#     c(x), 1.96*info$se, 
-#     d$N0.effective, d$N0, d$N0.effective/d$N0,
-#     d$T0.effective, d$T0, d$T0.effective/d$T0, 
-#     d$N1, d$T1)
-# }
+function format(x::synthdid_est1)
+  info = summary(x)
+  d = info.dimensions
+  summary_result = "synthdid: $(estimate) +- $(1.96 * info.se). Effective N0/N0 = $(d.N0_efective)/$(d.N0)~$(d.N0_efective / d.N0). Effective T0/T0 = $(d.T0_efective)/$(d.T0)~$(d.T0_efective/d.T0). N1,T1 = $(d.N1), $(d.T1)."
+  return summary_result
+end
+

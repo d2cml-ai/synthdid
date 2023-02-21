19d693793fcaf5318dee8cc5357eab40a7d68b5c
tjhon <fr.jhonk@gmail.com>
2023-02-21 12:44:48 -0500

    update

--- src/setup.jl
+++ src/setup.jl
@@ -11,10 +11,10 @@ include("vcov.jl")
 include("plots.jl")
 include("summary.jl")
 
-setup_data = panel_matrices(data("california_prop99"))
+# setup_data = panel_matrices(data("california_prop99"));
 
-Y = setup_data.Y
-N0 = setup_data.N0
-T0 = setup_data.T0
+# Y = setup_data.Y;
+# N0 = setup_data.N0;
+# T0 = setup_data.T0;
 
-tau_hat = synthdid_estimate(Y, N0, T0);
+# tau_hat = synthdid_estimate(Y, N0, T0);
